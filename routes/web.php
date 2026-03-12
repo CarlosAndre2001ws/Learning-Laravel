0cda6de680faaeb665e71c1cb0c958dfa5d3ae9b
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-03-12 13:34:13 -0300

    simple middleware tests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/mySecretToken', [App\Http\Controllers\MySecretTokenController::class, 'index'])->name('index')
+->middleware(EnsureTokenIsValid::class);
